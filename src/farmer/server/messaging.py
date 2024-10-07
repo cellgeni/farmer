bace8509fe263c1d79e7b4bdfaa9d68a5a6c63e1
ash <ah37@sanger.ac.uk>
2024-10-07 12:12:54 +0100

    Tidy efficiency formatting in no-memlimit case
    "Efficiency: 0.00% of 1 CPUs, (no memlimit set)." looks untidy, let's
    remove the comma in that case.

--- src/farmer/server/messaging.py
+++ src/farmer/server/messaging.py
@@ -5,7 +5,7 @@ def format_efficiency(job: dict) -> str:
         Efficiency: 2.30% of 4 CPUs, 4.50% of 4 G mem
     """
     memlimit = job["MEMLIMIT"]
-    return "Efficiency: " + ", ".join([
+    return "Efficiency: " + "".join([
         f"{job['AVERAGE_CPU_EFFICIENCY']} of {job['NALLOC_SLOT']} CPUs",
-        f"{job['MEM_EFFICIENCY']} of {memlimit} mem" if memlimit else "(no memlimit set)",
+        f", {job['MEM_EFFICIENCY']} of {memlimit} mem" if memlimit else " (no memlimit set)",
     ])
