cellgeni/farmer

commit 6ad6c37821ab4f0cffe2436dda23c90f52b38edc

Move Slack listener matchers into messaging module

src/farmer/server/__init__.py
@@ -20,7 +20,7 @@ from slack_sdk.models.views import View
 from slack_sdk.web.async_client import AsyncWebClient
 
 from farmer.server import messaging
-
+from farmer.server.messaging import dms_only, received_by_bot
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s][%(levelname)s] %(message)s")
 
@@ -138,42 +138,6 @@ def make_job_blocks(job: dict) -> list[Block]:
 # real init of the slack app with bot token (add socket handler to be explicit?)
 slack_bot = AsyncApp(token=os.environ.get("SLACK_BOT_TOKEN"))
 slack_app_client = AsyncWebClient(os.environ.get("SLACK_APP_TOKEN"))
-
-
-async def dms_only(message):
-    """Filter for messages sent in a DM between two users.
-
-    This is a listener matcher, see the Slack Bolt docs:
-    <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
-    """
-    return message.get("channel_type") == "im"
-
-
-async def received_by_bot(body):
-    """Filter for events received due to a bot authorization.
-
-    When a Slack app is installed, it can receive both its own events
-    (e.g. people sending messages to a bot user) and events relating to
-    the user who installed the app (e.g. direct messages sent to that
-    user). We want to ignore the latter case, so we only get events that
-    were received because our own bot user observed them.
-
-    This is a listener matcher, see the Slack Bolt docs:
-    <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
-    """
-    # TODO: we should cache the result of this somewhere
-    #   (it's not an urgent problem, since Slack does not ratelimit
-    #   auth.test heavily, but still good practice)
-    ourself = await slack_bot.client.auth_test()
-    auths = body.get("authorizations")
-    # this list will contain at most one element
-    # https://api.slack.com/changelog/2020-09-15-events-api-truncate-authed-users
-    if isinstance(auths, list) and len(auths) == 1 and auths[0].get("user_id") == ourself["user_id"]:
-        return True
-    # we need to check whether we saw the event for multiple reasons...
-    # must use the app token for this API call
-    more_auths = await slack_app_client.apps_event_authorizations_list(event_context=body.get("event_context"))
-    return any(auth["user_id"] == ourself["user_id"] for auth in more_auths["authorizations"])
 
 
 @slack_bot.message("(?i)ping", [dms_only, received_by_bot])

src/farmer/server/messaging.py
@@ -1,3 +1,6 @@
+from farmer.server import slack_bot, slack_app_client
+
+
 def format_efficiency(job: dict) -> str:
     """Format job CPU/memory efficiency stats.
 
@@ -9,3 +12,39 @@ def format_efficiency(job: dict) -> str:
         f"{job['AVERAGE_CPU_EFFICIENCY']} of {job['NALLOC_SLOT']} CPUs",
         f", {job['MEM_EFFICIENCY']} of {memlimit} mem" if memlimit else " (no memlimit set)",
     ])
+
+
+async def dms_only(message):
+    """Filter for messages sent in a DM between two users.
+
+    This is a listener matcher, see the Slack Bolt docs:
+    <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
+    """
+    return message.get("channel_type") == "im"
+
+
+async def received_by_bot(body):
+    """Filter for events received due to a bot authorization.
+
+    When a Slack app is installed, it can receive both its own events
+    (e.g. people sending messages to a bot user) and events relating to
+    the user who installed the app (e.g. direct messages sent to that
+    user). We want to ignore the latter case, so we only get events that
+    were received because our own bot user observed them.
+
+    This is a listener matcher, see the Slack Bolt docs:
+    <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
+    """
+    # TODO: we should cache the result of this somewhere
+    #   (it's not an urgent problem, since Slack does not ratelimit
+    #   auth.test heavily, but still good practice)
+    ourself = await slack_bot.client.auth_test()
+    auths = body.get("authorizations")
+    # this list will contain at most one element
+    # https://api.slack.com/changelog/2020-09-15-events-api-truncate-authed-users
+    if isinstance(auths, list) and len(auths) == 1 and auths[0].get("user_id") == ourself["user_id"]:
+        return True
+    # we need to check whether we saw the event for multiple reasons...
+    # must use the app token for this API call
+    more_auths = await slack_app_client.apps_event_authorizations_list(event_context=body.get("event_context"))
+    return any(auth["user_id"] == ourself["user_id"] for auth in more_auths["authorizations"])

commit 91dfa2d366a8b81a4e2a742c9e5345e3a40f026a

Move from credentials.json to .env.{server,reporter}
Additionally, fix an issue probably introduced in 6ad6c37 (Move Slack
listener matchers into messaging module, 2024-10-07) where the server
could not start due to circular imports.

pyproject.toml
@@ -10,6 +10,7 @@ dependencies = [
     "fastapi-websocket-rpc>=0.1.27",
     "fastapi>=0.114.0",
     "pydantic>=2.9.1",
+    "python-dotenv>=1.0.1",
     "slack-bolt==1.20.0",
     "uvicorn>=0.30.6",
 ]

src/farmer/reporter/__init__.py
@@ -14,9 +14,11 @@ from datetime import datetime
 from typing import Any, Sequence, Literal
 
 import aiorun
+from dotenv import load_dotenv
 from fastapi_websocket_rpc import RpcMethodsBase, WebSocketRpcClient
 
 
+load_dotenv(".env.reporter")
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s][%(levelname)s] %(message)s")
 
 

src/farmer/server/__init__.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 
 import aiorun
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI, BackgroundTasks
 from fastapi_websocket_rpc import WebsocketRPCEndpoint, RpcChannel
 from fastapi_websocket_rpc.rpc_channel import RpcCaller
@@ -20,8 +21,9 @@ from slack_sdk.models.views import View
 from slack_sdk.web.async_client import AsyncWebClient
 
 from farmer.server import messaging
-from farmer.server.messaging import dms_only, received_by_bot
 
+
+load_dotenv(".env.server")
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s][%(levelname)s] %(message)s")
 
 
@@ -87,13 +89,6 @@ LSF_IGNORED_KEYS = {
 RECENTLY_HANDLED_JOBS = set()
 
 
-# Shhhhhh: load credentials
-# !! please don't commit credentials.json !!
-for k, v in json.load(open("credentials.json", mode="rt")).items():
-    logging.info(f"Loading '{k}'")
-    os.environ[k] = v
-
-
 # stupidest way to get who you are asking jobs for "jobs for XXXX"
 # why people want to know others people's jobs I ask?
 # well service accounts for one, nosy people too?
@@ -138,9 +133,10 @@ def make_job_blocks(job: dict) -> list[Block]:
 # real init of the slack app with bot token (add socket handler to be explicit?)
 slack_bot = AsyncApp(token=os.environ.get("SLACK_BOT_TOKEN"))
 slack_app_client = AsyncWebClient(os.environ.get("SLACK_APP_TOKEN"))
+matchers = messaging.SlackMatchers(slack_bot.client, slack_app_client)
 
 
-@slack_bot.message("(?i)ping", [dms_only, received_by_bot])
+@slack_bot.message("(?i)ping", [matchers.dms_only, matchers.received_by_bot])
 async def message_ping(ack, say):
     await ack()
     await say("hello! I am Farmer.")
@@ -149,7 +145,7 @@ async def message_ping(ack, say):
 
 # ahoy this is handeling the message that has the word JOBS in it
 # main use for now...
-@slack_bot.message("(?i)jobs", [dms_only, received_by_bot])
+@slack_bot.message("(?i)jobs", [matchers.dms_only, matchers.received_by_bot])
 async def message_jobs(message, say):
     logging.info(f"message {message}")
     # who's pinging?
@@ -209,7 +205,7 @@ async def handle_job_details(ack, body, logger, client):
 
 # sending a message that we don't know?
 # tell them we don't know
-@slack_bot.event("message", [dms_only, received_by_bot])
+@slack_bot.event("message", [matchers.dms_only, matchers.received_by_bot])
 async def handle_message_events(body, logger):
     logger.warning("Unknown message")
     logger.warning(body)

src/farmer/server/messaging.py
@@ -1,4 +1,4 @@
-from farmer.server import slack_bot, slack_app_client
+from slack_sdk.web.async_client import AsyncWebClient
 
 
 def format_efficiency(job: dict) -> str:
@@ -14,37 +14,41 @@ def format_efficiency(job: dict) -> str:
     ])
 
 
-async def dms_only(message):
-    """Filter for messages sent in a DM between two users.
+class SlackMatchers:
+    def __init__(self, bot_client: AsyncWebClient, app_client: AsyncWebClient) -> None:
+        self._bot_client = bot_client
+        self._app_client = app_client
 
-    This is a listener matcher, see the Slack Bolt docs:
-    <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
-    """
-    return message.get("channel_type") == "im"
+    async def dms_only(self, message):
+        """Filter for messages sent in a DM between two users.
 
+        This is a listener matcher, see the Slack Bolt docs:
+        <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
+        """
+        return message.get("channel_type") == "im"
 
-async def received_by_bot(body):
-    """Filter for events received due to a bot authorization.
+    async def received_by_bot(self, body):
+        """Filter for events received due to a bot authorization.
 
-    When a Slack app is installed, it can receive both its own events
-    (e.g. people sending messages to a bot user) and events relating to
-    the user who installed the app (e.g. direct messages sent to that
-    user). We want to ignore the latter case, so we only get events that
-    were received because our own bot user observed them.
+        When a Slack app is installed, it can receive both its own events
+        (e.g. people sending messages to a bot user) and events relating to
+        the user who installed the app (e.g. direct messages sent to that
+        user). We want to ignore the latter case, so we only get events that
+        were received because our own bot user observed them.
 
-    This is a listener matcher, see the Slack Bolt docs:
-    <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
-    """
-    # TODO: we should cache the result of this somewhere
-    #   (it's not an urgent problem, since Slack does not ratelimit
-    #   auth.test heavily, but still good practice)
-    ourself = await slack_bot.client.auth_test()
-    auths = body.get("authorizations")
-    # this list will contain at most one element
-    # https://api.slack.com/changelog/2020-09-15-events-api-truncate-authed-users
-    if isinstance(auths, list) and len(auths) == 1 and auths[0].get("user_id") == ourself["user_id"]:
-        return True
-    # we need to check whether we saw the event for multiple reasons...
-    # must use the app token for this API call
-    more_auths = await slack_app_client.apps_event_authorizations_list(event_context=body.get("event_context"))
-    return any(auth["user_id"] == ourself["user_id"] for auth in more_auths["authorizations"])
+        This is a listener matcher, see the Slack Bolt docs:
+        <https://tools.slack.dev/bolt-python/concepts/listener-middleware>
+        """
+        # TODO: we should cache the result of this somewhere
+        #   (it's not an urgent problem, since Slack does not ratelimit
+        #   auth.test heavily, but still good practice)
+        ourself = await self._bot_client.auth_test()
+        auths = body.get("authorizations")
+        # this list will contain at most one element
+        # https://api.slack.com/changelog/2020-09-15-events-api-truncate-authed-users
+        if isinstance(auths, list) and len(auths) == 1 and auths[0].get("user_id") == ourself["user_id"]:
+            return True
+        # we need to check whether we saw the event for multiple reasons...
+        # must use the app token for this API call
+        more_auths = await self._app_client.apps_event_authorizations_list(event_context=body.get("event_context"))
+        return any(auth["user_id"] == ourself["user_id"] for auth in more_auths["authorizations"])