649a2b4e2296458c0cebb6c177d26f95687b9287
ash <ah37@sanger.ac.uk>
2024-10-14 12:18:57 +0100

    Show memory usage even if no memlimit was set

--- src/farmer/server/messaging.py
+++ src/farmer/server/messaging.py
@@ -8,9 +8,10 @@ def format_efficiency(job: dict) -> str:
         Efficiency: 2.30% of 4 CPUs, 4.50% of 4 G mem
     """
     memlimit = job["MEMLIMIT"]
-    return "Efficiency: " + "".join([
+    max_mem = job["MAX_MEM"].removesuffix("bytes")
+    return "Efficiency: " + ", ".join([
         f"{job['AVERAGE_CPU_EFFICIENCY']} of {job['NALLOC_SLOT']} CPUs",
-        f", {job['MEM_EFFICIENCY']} of {memlimit} mem" if memlimit else " (no memlimit set)",
+        f"{job['MEM_EFFICIENCY']} of {memlimit} mem" if memlimit else f"{max_mem} mem (no memlimit set)",
     ])
 
 
